34b1b6599d5d79871766136c8a3f12857533b063
DedSec <31364757+Armin-Nosrati@users.noreply.github.com>
2017-08-26 23:39:58 +0430

    Add files via upload

--- passGen.py
+++ passGen.py
@@ -1,9 +1,12 @@
+#imports
 import os
 
+
+#header
 firstName = raw_input('Please Enter First Name:')
 lastName = raw_input ('Please Enter Last Name: ')
 
-
+#Codes
 file = open ('pass.txt', 'w')
 file.write(firstName + '\n')
 file.write(lastName + '\n')
@@ -26,10 +29,17 @@ f = 999
 while (f < 2000):
 	f = f + 1
 	file.write(lastName + firstName + str(f) + '\n')
-
+f = 999
+while (f < 2000):
+	f = f + 1
+	file.write(str(f) + firstName + lastName + '\n')
+f = 999
+while (f < 2000):
+	f = f + 1
+	file.write(str(f) + lastName + firstName + '\n')
 file.close()
 
-
+#footer
 print(' ')
 print(' ')
 print(' ')
